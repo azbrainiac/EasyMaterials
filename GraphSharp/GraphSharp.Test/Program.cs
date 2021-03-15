81212555b9b7f953030d88aa2783112700ee6965
vasiliy azorkin <vasiliy.azorkin@stuzo.com>
2021-03-15 22:51:16 +0200

    Add init for DFS

---
+++ GraphSharp/GraphSharp.Test/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using GraphSharpLib;
+
+namespace GraphSharp.Test
+{
+
+    public class Entity{
+        public string Title { get; private set; }
+        public decimal Amount { get; private set; }
+
+        public static Entity Create(string title, decimal amount)
+        {
+            return new Entity{
+                Title = title,
+                Amount = amount
+            };            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var graph = new Graph<Entity>();
+            
+            var first = Entity.Create("first", 2.34m);
+            var second = Entity.Create("second", 3.4m);
+            var third = Entity.Create("third", 77.28m);
+            var forth = Entity.Create("forth", 66.11m);
+
+            graph.AddEdge(first, second);
+            graph.AddEdge(first, third);
+            graph.AddEdge(second, forth);
+            graph.AddEdge(forth, first);
+
+            graph.DfsTraverse(second, vertex => Console.WriteLine($"{vertex.Title} - {vertex.Amount}"));
+            
+            // var graph = new AcyclicDirectedGraph<int>();
+            // graph.AddEdge(1, 2);
+            // graph.AddEdge(2, 3);
+            // graph.AddEdge(2, 5);
+            // graph.AddEdge(3, 4);
+            // graph.AddEdge(5, 8);
+            // graph.AddEdge(4, 6);
+            // graph.AddEdge(4, 7);
+            // graph.AddEdge(4, 5);
+            // graph.AddEdge(5, 6);
+            // graph.DfsTraverse(2, vertex => Console.WriteLine(vertex));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GraphSharp/GraphSharpLib/Graph.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphSharpLib
+{
+    public class Graph<T>
+    {
+        private Dictionary<T, int> Vertices { get; }
+
+        private List<List<T>> VerticesEdges { get; }
+        private bool[] _visited;
+        private Action<T> _visit;
+        
+        public Graph()
+        {
+            Vertices = new Dictionary<T, int>();
+            VerticesEdges = new List<List<T>>();
+        }
+
+        public void AddEdge (T from, T to){
+            if(!Vertices.TryGetValue(from, out var index)){                
+                var edges = new List<T>{ to };
+                Vertices.Add(from, Vertices.Count);
+                VerticesEdges.Add(edges);
+            }
+            else{
+                var edges = VerticesEdges[index];
+                edges.Add(to);
+            }
+            
+            if(!Vertices.TryGetValue(to, out index)){                
+                var edges = new List<T>();
+                Vertices.Add(to, Vertices.Count);
+                VerticesEdges.Add(edges);
+            }            
+        }
+
+        public IEnumerable<T> GetSorted(){
+            int[] inDegree = new int[Vertices.Count];
+            foreach(var v in Vertices){
+                                
+            }
+            return null;
+        }
+
+        public void DfsTraverse(T source, Action<T> visit){
+            _visited = new bool[Vertices.Count];
+            _visit = visit;
+            Traverse(source);
+        }
+
+        private void Traverse(T source){
+            _visit(source);
+            int index = Vertices[source]; 
+
+            _visited[index] = true;
+
+            foreach(var linked in VerticesEdges[index]){
+                int vertexIndex = Vertices[linked];
+                if(!_visited[vertexIndex]){
+                    Traverse(linked);
+                }
+            }
+        } 
+    }
+}
